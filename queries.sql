74fb7fe67095e19e9557d4abcfdd1467dbb4968f
abdessittir harkaty <abdessattarharkaty@gmail.com>
2023-01-18 12:45:45 +0100

    Insert data info animals table and update it

--- queries.sql
+++ queries.sql
@@ -7,3 +7,44 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE NOT name = 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;SELECT date_of_birth FROM animals WHERE name='Agumon' OR name='Pikachu';
+
+
+update animals set species='unspecified';
+rollback;
+select * from animals;
+
+update animals set species='digimon' where name like '%mon';
+commit;
+update animals set species='pokemon' where species is NULL;
+commit;
+select * from animals;
+
+
+begin;
+  delete from animals where date_of_birth >= '2022-01-01';
+  savepoint pdate;
+  update animals set weight_kg = weight_kg * -1;
+  rollback to pdate;
+  update animals set weight_kg = weight_kg * -1 where weight_kg < 0;
+commit;
+select * from animals;
+
+select count(*) from animals;
+
+select count(*) from animals where escape_attempts=0;
+
+select avg(weight_kg) from animals;
+
+select avg(escape_attempts) as neutered from animals where neutered=true;
+select avg(escape_attempts) as not_neutered from animals where neutered=false;
+
+select max(weight_kg) from animals where neutered=true;
+select min(weight_kg) from animals where neutered=true;
+select max(weight_kg) from animals where neutered=false;
+select min(weight_kg) from animals where neutered=false;
+
+select avg(escape_attempts) as neutered from animals
+ where neutered=true and date_of_birth > '1990-01-01' and date_of_birth < '2000-01-01';
+select avg(escape_attempts) as not_neutered from animals
+ where neutered=false and date_of_birth > '1990-01-01' and date_of_birth < '2000-01-01';
+  
